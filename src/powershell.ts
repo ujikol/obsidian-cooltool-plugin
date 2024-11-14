212976d5721222008dc5ab5764eafb5f1bb13215
Michael Dauer <m.dauer@sec-consult.com>
2024-11-14 12:00:31 +0100

    Fixed issue with long parameters in powershell

--- src/powershell.ts
+++ src/powershell.ts
@@ -2,8 +2,12 @@
 // Install-Module -Name PowerShellGet -Scope CurrentUser -Force -AllowClobber
 // Install-Module -Name MicrosoftTeams -Scope CurrentUser -Force -AllowClobber
 // Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+
+// const { btoa } = require("node:buffer")
+
+
 export async function executePowerShellCommand(cmd: string): Promise<[boolean, string]> {
-    const powershellPath = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
     const execute = (args: string): Promise<[boolean, string]> => {
         return new Promise((resolve, reject) => {
             require('child_process').exec(`powershell ${args}`, { shell: true, detached: true }, (error: any, stdout: string, stderr: string) => {
@@ -28,6 +32,7 @@ export async function executePowerShellCommand(cmd: string): Promise<[boolean, s
 
 export function pssavpar(value: string): string {
     const utf8Bytes = new TextEncoder().encode(value)
-    const base64 = btoa(String.fromCharCode(...utf8Bytes))
+    // const base64 = btoa(String.fromCharCode(...utf8Bytes))
+    const base64 = btoa([].reduce.call(new Uint8Array(utf8Bytes),function(p:any,c:any){return p+String.fromCharCode(c)},''))
     return `([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64}')))`
 }
